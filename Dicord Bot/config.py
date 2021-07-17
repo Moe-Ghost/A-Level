bdafc1ff7ff35c8c207a39fb1cc619c7e2890630
Lardie <trofimchyk61082@gmail.com>
2021-07-17 22:20:47 +0300

    create bot

--- Dicord Bot/config.py
+++ Dicord Bot/config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODY1MjczNTQxNDk4NTAzMTc5.YPBmyA.0CAMzIfY4xqdsQPObT30uNaOwPw',
+    'token': 'ODY1MjczNTQxNDk4NTAzMTc5.YPBmyA.igQLA9Ih9amnjuMkYhgclINNjL8',
     'bot': 'testBOT',
     'id': 865273541498503179,
     'prefix': '$'
